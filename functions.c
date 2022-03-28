7412c1050320559ebea215ffde222be27e6d4435
bannysha <manyapubannysha2000@gmail.com>
2022-03-28 16:05:18 +0530

    python

---
+++ functions.c
@@ -0,0 +1,18 @@
+**FUNCTION CREATION**
+ In python a function is defined using  def keyword.
+def my_function():
+  print("hello from a function")
+
+
+**FUNCTION ARGUMRNTS**
+
+def my_function(fname):
+    print(fname + "banny")
+
+    
+my_function("banny")
+bannybanny
+my_function("arveni")
+arvenibanny
+my_function("sai")
+saibanny
