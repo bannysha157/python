da092da83167cbf9ae75b27a45dc92df4dc4a173
bannysha <manyapubannysha2000@gmail.com>
2022-03-28 21:14:34 +0530

    python

---
+++ escapesequence.c
@@ -0,0 +1,19 @@
+#escape sequence
+print("\escape sequence\n")
+o/p  \escape sequence
+
+msg = '\'strings areimmutable\''
+print(msg)
+o/p 'strings areimmutable'
+
+msg ='\"strings are immutable\"'
+print(msg)
+o/p "strings are immutable"
+
+msg= 'strings\tare\timmutable'  #\t=tab spece
+print(msg)
+ o/p strings	are	immutable
+
+msg= 'strings \\immutable' #\\=it gives \
+print(msg)
+o/p strings \immutable
---
+++ formatsequence.c
@@ -0,0 +1,16 @@
+a=12.000
+print(a)
+o/p 12.0
+print("int of a is : %d"%a)
+o/p int of a is : 12
+print("float of a is %d"%a)
+o/p float of a is 12
+print("float value : %0.2f"%a)
+o/p float value : 12.00
+a='cats'
+b='dogs'
+c=3
+d=4
+print("there are %d %s and %d %s "%(c,a,d,b))
+o/p there are 3 cats and 4 dogs 
+
